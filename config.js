77f13237cb90374387c1a868a0bbb3578a419668
zhhangfly <331161787@qq.com>
2020-02-01 17:43:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -30,8 +30,8 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "准备好了木有",
-        play: "慢慢喜欢你",
+        turn_on: "开始了哦",
+        play: "慢慢喜欢你，就这样老去",
         bannar_coming: "b-ling b-ling",
         balloons_flying: "飘~",
         cake_fadein: "蛋糕？",
